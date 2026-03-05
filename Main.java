3a712b4481ab4c3bfcb624bd71d521125b47f78b
Akram khan <khanakram40507@gmail.com>
2026-03-05 15:00:23 +0530

    dynamic intialization of array

--- Main.java
+++ Main.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 class Main{
     public static void main(String[] args) {
         int arr[] = new int[5];
-        arr[0] = -10;
-        arr[1] = 20;
-        arr[2] = 30;    
-        arr[3] = 40;
-        arr[4] = 50;
+            Scanner sc = new Scanner(System.in);
+            for(int i=0; i<arr.length; i++){
+                arr[i] = sc.nextInt();
+            }
         for(int i=0; i<arr.length; i++){  // here length is variable which is used to find the length of array , in case of string it is length() method.
-            System.out.println(arr[i]);
+            System.out.print(arr[i] + " ");
         }       
 
 }
